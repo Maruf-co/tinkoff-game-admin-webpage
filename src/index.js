73b1c0cbda7c1df1c4e217950ff5ff3a58569c32
Dirakon <609700zxc@mail.ru>
2022-06-07 12:49:18 +0300

    downgraded react to 17 version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+    root
 );
 
 // If you want to start measuring performance in your app, pass a function
